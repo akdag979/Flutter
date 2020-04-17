7ea50beaeb8e6ae111e7d1031e333f332ddf15d4
Levent Akdag <akdaglevent@gmail.com>
2020-04-17 15:03:51 -0700

    Update main.dart
    sample for 2 Column - 2 Row using by Column widget and Row widget

--- main.dart
+++ main.dart
@@ -10,12 +10,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Hello Flutter',
-            home: Scaffold(
-                     appBar: AppBar(title: Text('Hello Flutter', style: TextStyle(fontSize: 30.0),), backgroundColor: Colors.red),
-                     body: Center(child: Text('Hello Budy',style: TextStyle(color: Colors.red,fontSize: 35.0)),)
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Hello Flutter',
+      home: Scaffold(
+          appBar: AppBar(
+              title: Text(
+                'Hello Flutter',
+                style: TextStyle(fontSize: 30.0),
+              ),
+              backgroundColor: Colors.red),
+          body: Center(
+            child: Container(
+              padding: EdgeInsets.only(left: 10.0,top: 40.0),
+              alignment: Alignment.center,
+              color: Colors.red,
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text(
+                          'Random Name ;',
+                          style: TextStyle(fontSize: 30.0, color: Colors.white),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          'Random Number ;',
+                          style: TextStyle(fontSize: 30.0, color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text(
+                          'random name',
+                          style: TextStyle(fontSize: 30.0, color: Colors.white),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          'random number',
+                          style: TextStyle(fontSize: 30.0, color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )),
     );
   }
 }
