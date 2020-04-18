c7b68b2a20e81b40ad733a58e75d9827ac9a5120
Levent Akdag <akdaglevent@gmail.com>
2020-04-18 12:26:37 -0700

    Update main.dart
    contact list sample with FloatActionButton and SnackBarAction Widgets

--- main.dart
+++ main.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,24 +13,44 @@ void main() {
         title: Text("Sample List"),
       ),
       body: getListView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          debugPrint("FloaActionButton Cicked");
+        },
+        child: Icon(Icons.add),
+        tooltip: 'Add more item',
+      ),
     ),
   ));
 }
 
+void showSnackBar(BuildContext context, String item){
+  var snackBar = SnackBar(
+    content: Text("You just clicked $item."),
+    action: SnackBarAction(
+      label: "UNDO",
+      onPressed: (){
+        debugPrint('permorming dummy ANY Operation');
+      },
+    ),
+  );
+  Scaffold.of(context).showSnackBar(snackBar);
+}
 /*List<String> getListElements() {
-  var items = List<String>.generate(100, (itemno) => "item $itemno");
+  var items = List<String>.generate(100, (contactid) => "Contact $contactid");
   return items;
 }*/
 
 Widget getListView() {
   // var listItems = getListElements(); // also possible
-  var listItems = List<String>.generate(100, (itemno) => "item $itemno");
+  var listItems = List<String>.generate(100, (contactid) => "Contact $contactid");
   var listView = ListView.builder(itemBuilder: (context, index) {
     return ListTile(
       leading: Icon(Icons.arrow_right),
       title: Text(listItems[index]),
       onTap: () {
-        debugPrint('${listItems[index]} clicked');
+        debugPrint('You just clicked ${listItems[index]}.');
+        showSnackBar(context, listItems[index]);
       },
     );
   });
