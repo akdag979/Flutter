1f0135ca52beab22d07192b2e47375999a992754
Levent Akdag <akdaglevent@gmail.com>
2020-04-17 20:25:19 -0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -2,7 +2,21 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// first update pubspec.yaml as below
+/*
+dependencies:
+flutter:
+sdk: flutter
+
+cupertino_icons: ^0.1.2
+english_words: ^3.1.0   # add this line
+*/
+// Copyright 2020 Levent Akdag. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,12 +30,12 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
               title: Text(
                 'Hello Flutter',
-                style: TextStyle(fontSize: 30.0),
+                style: TextStyle(fontSize: 30.0, color: Colors.red),
               ),
-              backgroundColor: Colors.red),
+              backgroundColor: Colors.orange),
           body: Center(
             child: Container(
-              padding: EdgeInsets.only(left: 10.0,top: 40.0),
+              padding: EdgeInsets.only(left: 10.0, top: 40.0),
               alignment: Alignment.center,
               color: Colors.red,
               child: Column(
@@ -30,7 +44,7 @@ class MyApp extends StatelessWidget {
                     children: <Widget>[
                       Expanded(
                         child: Text(
-                          'Random Name ;',
+                          'Random Word ;',
                           style: TextStyle(fontSize: 30.0, color: Colors.white),
                         ),
                       ),
@@ -45,23 +59,85 @@ class MyApp extends StatelessWidget {
                   Row(
                     children: <Widget>[
                       Expanded(
-                        child: Text(
-                          'random name',
-                          style: TextStyle(fontSize: 30.0, color: Colors.white),
-                        ),
+                        child: RandomWords(),
                       ),
                       Expanded(
                         child: Text(
-                          'random number',
+                          'Random Number ;',
                           style: TextStyle(fontSize: 30.0, color: Colors.white),
                         ),
                       ),
                     ],
                   ),
+                  RandomImaage(),
+                  GenerateOneMore(),
                 ],
               ),
             ),
           )),
     );
   }
+}
+
+/// Random English words code starts here
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final WordPair wordPair = WordPair.random();
+    return Text(
+      wordPair.asPascalCase,
+      style: TextStyle(color: Colors.white, fontSize: 30.0),
+    );
+  }
+}
+
+/// image class is below
+class RandomImaage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = AssetImage('lib/images/Random_Order.png');
+    Image image = Image(
+      image: assetImage,
+      width: 250.0,
+      height: 250.0,
+    );
+    return Container(
+      child: image,
+    );
+  }
+}
+
+/// button goes below
+class GenerateOneMore extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 30.0),
+      width: 250.0,
+      height: 100,
+      child: RaisedButton(
+        color: Colors.orange,
+        child: Text(
+          'Generate Random',
+          style: TextStyle(fontSize: 30.0, color: Colors.white),
+        ),
+        elevation: 9.0,
+        onPressed: () => generateRandom(context),
+      ),
+    );
+  }
+}
+
+void generateRandom(BuildContext context) {
+  var alertDialog = AlertDialog(
+    title: Text("Generated"),
+    content: Text('Hope to see you again'),
+  );
+  showDialog(context: context, builder: (BuildContext context) => alertDialog);
 }
