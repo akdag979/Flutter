3002cf22d41e77beaa6f675661d9ebe2f78ba1ca
Levent Akdag <akdaglevent@gmail.com>
2020-04-23 19:40:48 -0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -2,57 +2,55 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Listing Sample",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Sample List"),
-      ),
-      body: getListView(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          debugPrint("FloaActionButton Cicked");
-        },
-        child: Icon(Icons.add),
-        tooltip: 'Add more item',
-      ),
-    ),
-  ));
-}
 
-void showSnackBar(BuildContext context, String item){
-  var snackBar = SnackBar(
-    content: Text("You just clicked $item."),
-    action: SnackBarAction(
-      label: "UNDO",
-      onPressed: (){
-        debugPrint('permorming dummy ANY Operation');
-      },
-    ),
+  runApp(
+      MaterialApp(
+        title: "Sateful App Example",
+        home: InputtoText(),
+      )
   );
-  Scaffold.of(context).showSnackBar(snackBar);
 }
-/*List<String> getListElements() {
-  var items = List<String>.generate(100, (contactid) => "Contact $contactid");
-  return items;
-}*/
 
-Widget getListView() {
-  // var listItems = getListElements(); // also possible
-  var listItems = List<String>.generate(100, (contactid) => "Contact $contactid");
-  var listView = ListView.builder(itemBuilder: (context, index) {
-    return ListTile(
-      leading: Icon(Icons.arrow_right),
-      title: Text(listItems[index]),
-      onTap: () {
-        debugPrint('You just clicked ${listItems[index]}.');
-        showSnackBar(context, listItems[index]);
-      },
+class InputtoText extends StatefulWidget {
+  @override
+  _InputtoTextState createState() {
+    return _InputtoTextState();
+  }
+}
+
+class _InputtoTextState extends State<InputtoText> {
+  String userInput = "";
+  
+  @override
+  Widget build(BuildContext context) {
+    debugPrint('UserInput Widget is created');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stateful App Sample with UserInput"),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              onSubmitted: (String userWrote) {
+                setState(() {
+                  debugPrint('SetState; This calls framework to restate the UserInput Widget');
+                  userInput = userWrote;
+                });
+              },
+            ),
+            Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Text("User wrote now $userInput", style: TextStyle(fontSize: 20.0),
+              ),
+            )
+          ],
+        ),
+      ),
     );
-  });
-  return listView;
+  }
 }
